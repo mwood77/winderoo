580a556e776fa61f969572cde8ffe7f239c21ddc
mwood77 <michael.wood@wetransfer.com>
2024-02-25 20:19:34 +0100

    remove unused import and bump FE version

--- src/angular/osww-frontend/src/app/api.service.ts
+++ src/angular/osww-frontend/src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, isDevMode } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
